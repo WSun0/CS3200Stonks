5015895d9cc3fe571e032c1c47b6e8e2b1304bd2
Franklin Chen <chen.frank@northeastern.edu>
2025-08-13 17:49:19 -0400

    Fixed issues with buttons

--- app/src/pages/22_Daniel_Home.py
+++ app/src/pages/22_Daniel_Home.py
@@ -7,10 +7,7 @@ from modules.nav import SideBarLinks
 st.set_page_config(layout = 'wide')
 
 # Show appropriate sidebar links for the role of the currently logged in user
-<<<<<<< HEAD
 # This function is correct, but the issue was in the button links below.
-=======
->>>>>>> 87a9cb74fbb54dc7b67742b3c21ad4b18b7a7bab
 SideBarLinks()
 
 st.title(f"Welcome {st.session_state['first_name']}.")
@@ -18,10 +15,7 @@ st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
-<<<<<<< HEAD
 # Fix the button links to point to Daniel's specific pages
-=======
->>>>>>> 87a9cb74fbb54dc7b67742b3c21ad4b18b7a7bab
 if st.button('View Backtest Data', 
              type='primary',
              use_container_width=True):
@@ -35,8 +29,4 @@ if st.button('Compare Stocks',
 if st.button('Import and Export Strategies', 
              type='primary',
              use_container_width=True):
-<<<<<<< HEAD
   st.switch_page('pages/25_Daniel_Strategies.py')
-=======
-  st.switch_page('pages/25_Daniel_Strategies.py')
->>>>>>> 87a9cb74fbb54dc7b67742b3c21ad4b18b7a7bab
--- app/src/pages/25_Daniel_Strategies.py
+++ app/src/pages/25_Daniel_Strategies.py
@@ -41,8 +41,4 @@ st.download_button(
     data=buffer.getvalue(),
     file_name="strategy_template.csv",
     mime="text/csv"
-<<<<<<< HEAD
 )
-=======
-)
->>>>>>> 87a9cb74fbb54dc7b67742b3c21ad4b18b7a7bab
