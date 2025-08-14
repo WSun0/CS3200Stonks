938792baad883d4759ddd591ed4120de647c8119
Jackerrigan <jack.kerrigan05@gmail.com>
2025-08-14 14:11:40 -0400

    Created Jamie pages
    creation of jamies login and pages

---
+++ app/src/pages/Jamie_Home.py
@@ -0,0 +1,32 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import streamlit as st
+from modules.nav import SideBarLinks
+
+st.set_page_config(layout = 'wide')
+
+# Show appropriate sidebar links for the role of the currently logged in user
+# This function is correct, but the issue was in the button links below.
+SideBarLinks()
+
+st.title(f"Welcome {st.session_state['first_name']}.")
+st.write('')
+st.write('')
+st.write('### What would you like to do today?')
+
+# Fix the button links to point to Daniel's specific pages
+if st.button('Diagnose Errors', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/Error_Diagnostics.py')
+
+if st.button('Manage Backups & Restores', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/Data_Backup.py')
+
+if st.button('Deploy Updates & Patches', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/Update_Patches.py')
